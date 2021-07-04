99adb427995f498d9d10d03f4afcb2bf118e659c
nugliar <hashan.hall@gmail.com>
2021-07-04 16:23:22 -0500

    change styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Clock } from './Clock'
 
+import githubIcon from './img/GitHub-Mark-Light-64px.png'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <Clock />
+      <div className='description'>
+        <a className='link' href='https://github.com/nugliar/calculator'>
+          <div>
+            <img src={githubIcon} alt='git' />
+          </div>
+          <p>25-5 Clock</p>
+        </a>
+      </div>
     </div>
   );
 }
